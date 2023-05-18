b5a9bb6692785048bb68a5893cb156cfe1379d17
Ulugbek-Akmalovich-Shodimurodov <99031894+Ulugbek-Akmalovich-Shodimurodov@users.noreply.github.com>
2023-05-18 17:01:32 +0500

    push

--- main.js
+++ main.js
@@ -20,6 +20,8 @@ Btn.addEventListener("click", (e)=>{
     let X_uzgaruvchilar = document.querySelector(".a").value.split(',')
     let Y_uzgaruvchilar = document.querySelector(".b").value.split(',')
 
+    console.log(X_uzgaruvchilar, Y_uzgaruvchilar);
+
     let sum_X = 0;
     let sum_X2 = 0;
     let sum_X3 = 0;
@@ -32,26 +34,28 @@ Btn.addEventListener("click", (e)=>{
 
     for (let i = 0; i < X_uzgaruvchilar.length; i++) {
 
-        sum_X += X_uzgaruvchilar[i] - 0;
-        sum_Y += Y_uzgaruvchilar[i] - 0;
-        sum_XY += 1 * X_uzgaruvchilar[i]*Y_uzgaruvchilar[i];
-        sum_XX += 1 * X_uzgaruvchilar[i]*X_uzgaruvchilar[i];
-        ssum_X += X_uzgaruvchilar[i] - 0;
+        sum_X += Number(X_uzgaruvchilar[i]) - 0;
+        sum_Y += Number(Y_uzgaruvchilar[i]) - 0;
+        sum_XY += 1 * Number(X_uzgaruvchilar[i]) * Number(Y_uzgaruvchilar[i]);
+        sum_XX += 1 * Number(X_uzgaruvchilar[i]) * Number(Y_uzgaruvchilar[i]);
+        ssum_X += Number(X_uzgaruvchilar[i]) - 0;
 
-        sum_X4 += Math.pow(X_uzgaruvchilar[i], 4);
-        sum_X3 += Math.pow(X_uzgaruvchilar[i], 3);
-        sum_X2 += Math.pow(X_uzgaruvchilar[i], 2);
-        sum_xxy += Math.pow(X_uzgaruvchilar[i], 2)*Y_uzgaruvchilar[i];
+        sum_X4 += Math.pow(Number(X_uzgaruvchilar[i]), 4);
+        sum_X3 += Math.pow(Number(X_uzgaruvchilar[i]), 3);
+        sum_X2 += Math.pow(Number(X_uzgaruvchilar[i]), 2);
+        sum_xxy += Math.pow(Number(X_uzgaruvchilar[i]), 2)*Number(Y_uzgaruvchilar[i]);
     }
         ssum_X *= ssum_X;
+
+        console.log(sum_X, sum_Y, sum_XY, sum_XX, ssum_X);
 
         let a = (X_uzgaruvchilar.length*sum_XY - sum_X*sum_Y)/(X_uzgaruvchilar.length*sum_XX - ssum_X);
         let b = (sum_Y - a*sum_X)/X_uzgaruvchilar.length;
 
 
-        let detA = sum_X4*(sum_X2*6-sum_X*sum_X) - sum_X3*(sum_X3*6-sum_X*sum_X2)+sum_X2*(sum_X*sum_X3-sum_X2*sum_X2);
-        let det_a = sum_xxy*(sum_X2*6-sum_X*sum_X)-sum_X3*(sum_XY * 6 - sum_X*sum_Y) + sum_X2*(sum_X*sum_XY-sum_Y*sum_X2);
-        let det_b = sum_X4*(sum_XY*6-sum_Y*sum_X)-sum_xxy*(sum_X3*6-sum_X*sum_X2)+sum_X2*(sum_X3*sum_Y-sum_XY*sum_X2);
+        let detA = sum_X4*(sum_X2*X_uzgaruvchilar.length-sum_X*sum_X) - sum_X3*(sum_X3*X_uzgaruvchilar.length-sum_X*sum_X2)+sum_X2*(sum_X*sum_X3-sum_X2*sum_X2);
+        let det_a = sum_xxy*(sum_X2*X_uzgaruvchilar.length-sum_X*sum_X)-sum_X3*(sum_XY * X_uzgaruvchilar.length - sum_X*sum_Y) + sum_X2*(sum_X*sum_XY-sum_Y*sum_X2);
+        let det_b = sum_X4*(sum_XY*X_uzgaruvchilar.length-sum_Y*sum_X)-sum_xxy*(sum_X3*X_uzgaruvchilar.length-sum_X*sum_X2)+sum_X2*(sum_X3*sum_Y-sum_XY*sum_X2);
         let det_c = sum_X4*(sum_X2*sum_Y-sum_X*sum_XY)-sum_X3*(sum_X3*sum_Y-sum_XY*sum_X2)+sum_xxy*(sum_X*sum_X3-sum_X2*sum_X2);
 
         let A = det_a/detA;
